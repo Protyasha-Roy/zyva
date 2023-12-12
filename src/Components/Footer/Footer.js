c76cd7e7875777d9f5e9cf891e8311e1af1f43e8
Protyasha-Roy <protyasharoy369@gmail.com>
2023-12-13 02:15:22 +0600

    footer style changed

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 
 const Footer = () => {
     return (
-        <section className='text-center mt-2'>
+        <section className='text-center mt-3'>
             <p className='sulphur-15 footer-text'>&copy; Designed and Developed by PROTYASHA</p>
         </section>
     );
