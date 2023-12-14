4333da71e520c224f7976d138ba1f6865dfbd293
Protyasha-Roy <protyasharoy369@gmail.com>
2023-12-14 18:00:31 +0600

    removed useEffect

--- src/Pages/Instructions/Instructions.js
+++ src/Pages/Instructions/Instructions.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
 import ReactPlayer from 'react-player/lib';
