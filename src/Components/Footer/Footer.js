6a10f9c763b422c746b98470ffbb04e12825e564
Protyasha-Roy <protyasharoy369@gmail.com>
2023-12-14 17:42:02 +0600

    added year

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,9 +2,11 @@ import React from 'react';
 import "./Footer.css";
 
 const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
     return (
         <section className='text-center mt-3'>
-            <p className='sulphur-15 footer-text'>&copy; Designed and Developed by PROTYASHA</p>
+            <p className='sulphur-15 footer-text'>&copy;{currentYear} Designed and Developed by Protyasha</p>
         </section>
     );
 };
